a949b017a726626cef526184f826c13807097c3b
Carlos <carlos.munoz@solera.com>
2022-10-31 11:39:41 +0100

    Frontend annadido

---
+++ OneDrive - Solera Holdings, Inc/Desktop/ASSESSMENT 3/BACKEND/src/test/java/com/assessment/flight/flight/assessment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.flight.flight.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
